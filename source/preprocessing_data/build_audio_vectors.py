076a6a5025016211978b2e93367352a204289004
Zara Mudassar <zamud3164@oslomet.no>
2024-04-30 16:15:50 +0200

    audio_vectors

--- source/preprocessing_data/build_audio_vectors.py
+++ source/preprocessing_data/build_audio_vectors.py
@@ -5,6 +5,8 @@ import pickle
 import pandas as pd
 import math
 
+print(librosa.__version__)
+
 
 print("This script will extract the data from the IEMOCAP dataset and save it as a CSV file.")
 print("The path for the IEMOCAP dataset on my laptop is: C:/Users/Zara Mudassar/OneDrive - OsloMet/Spring 1/4630/IEMOCAP/")
@@ -27,7 +29,7 @@ for sess in range(1, 6):
             orig_wav_vector, _sr = librosa.load(wav_file_path + orig_wav_file, sr=sr)
             orig_wav_file, file_format = orig_wav_file.split('.')
             for index, row in df_iemocap[df_iemocap['wav_file'].str.contains(orig_wav_file)].iterrows():
-                start_time, end_time, truncated_wav_file_name, emotion, val, act, dom = row['start_time'], row['end_time'], row['wav_file'], row['emotion'], row['val'], row['act'], row['dom']
+                start_time, end_time, truncated_wav_file_name, emotion = row['start_time'], row['end_time'], row['wav_file'], row['emotion']
                 start_frame = math.floor(start_time * sr)
                 end_frame = math.floor(end_time * sr)
                 truncated_wav_vector = orig_wav_vector[start_frame:end_frame + 1]
